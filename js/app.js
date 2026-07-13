6ed3c9b017fba9e9c710aacdb7337365205b2302
Michael Sherk <michaelsherk22@gmail.com>
2026-07-13 08:46:24 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 import { initRouter } from './router.js';
-import { initKitchenGame } from './games/kitchen.js';
-// later: import other games as you build them
+import { initKitchenGame } from './kitchen.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initRouter();
   initKitchenGame();
-  // initTravelGame();
-  // initStoriesGame();
-  // initPuzzleGame();
-  // initDressupGame();
 });
